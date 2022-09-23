e4fbf2f1e1429851f24c0eb54b0854e41325592e
Batuhan ÖZALP <batuhan.ozalp.9@yandex.com>
2022-09-23 15:54:22 +0300

    image push added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,61 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, Image, ActivityIndicator, Alert } from 'react-native';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Button from '../Components/Button';
 import * as ImagePicker from 'expo-image-picker';
+import { storage, getDownloadURL } from "firebase/app";
+import { addDoc, collection, doc, updateDoc } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../firebaseConfig';
+import uuid from 'react-native-uuid';
 
-const Musics = ({ navigation }) => {
-    const theme = useSelector((state) => state.theme.theme);
-    const [musicList, setMusicList] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [image, setImage] = useState(null);
+initializeApp(firebaseConfig);
+const Home = ({ navigation }) => {
+  const theme = useSelector((state) => state.theme.theme);
+  const [image, setImage] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [transferred, setTransferred] = useState(0);
 
-    const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            quality: 1,
-        });
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      quality: 1,
+    });
 
-        console.log(result);
+    console.log(result);
 
-        if (!result.cancelled) {
-            setImage(result.uri);
+    if (!result.cancelled) {
+      setImage(result.uri);
+      console.log(result.uri);
+    }
+  };
+
+
+  const uploadImage = async () => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError('Network request failed'));
+      };
+      xhr.responseType = 'blob';
+      xhr.open('GET', image, true);
+      xhr.send(null);
+    });
+    console.log(blob);
+    const storage = getStorage();
+    const fileRef = ref(storage, uuid.v4());
+    const result = await uploadBytes(fileRef, blob);
+
+    // We're done with the blob, close and release it
+    blob.close();
+
+    // return await getDownloadURL(fileRef);
+  };
+
+  return (
+    <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
+      <Text style={[styles.header_text, { color: theme.color }]}>Share image</Text>
+      <View>
+        <Button title="Select from gallery" onPress={pickImage} />
+      </View>
+      <View>
+        {image &&
+          <View style={{alignItems:'center'}}>
+            <Image source={{ uri: image }} style={styles.image} />
+            <Button title="Share image" onPress={uploadImage} />
+          </View>
         }
-    };
+      </View>
 
-    return (
-        <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
-            <Text style={[styles.header_text, { color: theme.color }]}>Share image</Text>
-            <View>
-                <Button title="Select from gallery" onPress={pickImage} />
-            </View>
-            <View>
-                {image && <Image source={{ uri: image }} style={styles.image} />}
-            </View>
-        </View>
-    );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-    },
-    header_text:
-    {
-        fontWeight: '800',
-        fontSize: 16,
-        paddingTop: 10,
-        paddingBottom: 50,
-    },
-    image:
-    {
-        width: 200,
-        height: 200,
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  header_text:
+  {
+    fontWeight: '800',
+    fontSize: 16,
+    paddingTop: 10,
+    paddingBottom: 50,
+  },
+  image:
+  {
+    width: 200,
+    height: 300,
+    marginBottom:10
+  },
 });
 
-export default Musics;
+export default Home;
--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -1,49 +1,96 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Dimensions, Alert } from 'react-native';
 import { useSelector } from 'react-redux';
 import MapView, { PROVIDER_GOOGLE, Marker, LatLng } from 'react-native-maps';
-//import Geolocation from '@react-native-community/geolocation';
+import * as Location from 'expo-location';
 
 const Map = () => {
     const theme = useSelector((state) => state.theme.theme);
-    /* const [coord, setCoord] = useState(LatLng);
- 
-     Geolocation.getCurrentPosition(
-         (c) =>
-             setCoord({
-                 latitude: c.coords.latitude,
-                 longitude: c.coords.longitude,
-             }),
-         (error) => console.log(error),
-         {
-             enableHighAccuracy: true,
-         },
-     );
- 
-     let myLocation = Location.getCurrentPositionAsync({})
-     const loc = {
-         latitude: myLocation.latitude,
-         longitude: myLocation.longitude,
-         latitudeDelta: myLocation.latitudeDelta,
-         longitudeDelta: myLocation.longitudeDelta
-     }
-     const coordDolmabahce = {
-         latitude: 41.0391683,
-         longitude: 28.9982707,
-         latitudeDelta: 0.01,
-         longitudeDelta: 0.01,
-     };*/
+    const [yourLocation, setLocation] = useState(null);
+    const [lat, setLat] = useState(null);
+    const [long, setLong] = useState(null);
+    const mapRef = useRef();
+
+    const getCurrentLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            Alert.alert('Permission to access location was denied');
+            return;
+        }
+
+        let location = await Location.watchPositionAsync(
+            {
+                distanceInterval: 10,
+            },
+            location => {
+                setLocation(location);
+                setLat(location.coords.latitude);
+                setLong(location.coords.longitude);
+                mapRef.current.animateToRegion({
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                });
+            },
+        );
+        // Alert.alert(location);
+    };
+
+    const updateUsersCurrentLocation = async location => {
+        //const docRef = doc(db, 'user', user.id);
+        await updateDoc(docRef, {
+            currentLocation: {
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+            },
+        });
+    };
+
+    useEffect(() => {
+        getCurrentLocation();
+    }, []);
+
+    /*const loc = {
+        latitude: myLocation.latitude,
+        longitude: myLocation.longitude,
+        latitudeDelta: myLocation.latitudeDelta,
+        longitudeDelta: myLocation.longitudeDelta
+    }*/
+    const coordDolmabahce = {
+        latitude: 41.0391683,
+        longitude: 28.9982707,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+    };
 
     return (
         <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
             <Text style={[styles.header, { color: theme.color }]}>
                 Map
             </Text>
+            <MapView
+                 initialRegion={coordDolmabahce}
+                //showsMyLocationButton
+                mapType="standard"
+               // ref={mapRef}
+                showsScale
+                showsUserLocation
+                style={styles.map}
+            //minZoomLevel={15}
+            >
+                
+                
+
+            </MapView>
             {/*  <MapView
                 style={styles.map}
                 provider={PROVIDER_GOOGLE}
                 initialRegion={coordDolmabahce}
             >
+               <Marker coordinate={{
+                    latitude: yourLocation.coords.latitude,
+                    longitude: yourLocation.coords.longitude,
+                }} />
+
                 {coord !== undefined && <Marker coordinate={coord} />}
     </MapView>*/}
         </View >
